b2040f47f05c027bdf661bbdd6e62fadef7dbc6f
stanley <stanley@zhihesoft.com>
2022-07-30 12:28:42 +0800

    export action* in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 
 export { ArgumentProvider } from "./lib/argument.provider";
-export { ActionEngine as MessageEngine } from "./lib/action.engine";
-export { ActionProcessor as MessageProcessor } from "./lib/action.processor";
-export { ActionRouting as MessageRouting } from "./lib/action.routing";
-export { ActionRoutingOption as MessageRoutingOption } from "./lib/action.routing.option";
+export { ActionEngine } from "./lib/action.engine";
+export { ActionProcessor } from "./lib/action.processor";
+export { ActionRouting } from "./lib/action.routing";
+export { ActionRoutingOption } from "./lib/action.routing.option";
