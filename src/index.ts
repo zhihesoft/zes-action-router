e90d7b93eebb16f19059af072de232c03f28ef45
stanley <stanley@zhihesoft.com>
2022-08-01 13:53:51 +0800

    rename actionengine to actionrouter

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 
 export { ActionHookType, ActionHook, ActionHookContext } from "./lib/action.hook";
-export { ActionRouter as ActionEngine } from "./lib/action.router";
+export { ActionRouter } from "./lib/action.router";
 export { ActionProcessor } from "./lib/action.processor";
 export { ActionRouting } from "./lib/action.routing";
 export { ActionRoutingOption } from "./lib/action.routing.option";
